b1a6300e1f573ca34bd82ea7474ee5cf86aa6ed5
s414june <s414june@gmail.com>
2024-04-12 22:01:49 +0800

    update gh page folder(doc)

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,3 +1,5 @@
+const path = require('path');
+
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   devtools: { enabled: true },
@@ -16,5 +18,10 @@ export default defineNuxtConfig({
         },
       ],
     },
+  },
+  nitro: {
+    output: {
+      publicDir: path.join(__dirname, 'docs')
+    }
   },
 });
