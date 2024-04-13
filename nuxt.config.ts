90607de5c243dc4adca8355cfd6b245dba963415
s414june <s414june@gmail.com>
2024-04-13 11:35:03 +0800

    debug config

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,13 +1,13 @@
-const path = require("path");
+import * as path from "path";
+import { fileURLToPath } from "url";
+
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
 
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  vite: {
-    resolve: {
-      alias: {
-        "@": "/path/to/src",
-      },
-    },
+  alias: {
+    "@": path.resolve(__dirname, "./"),
+    // 其他别名
   },
   devtools: { enabled: true },
   css: ["~/assets/css/main.css"],
