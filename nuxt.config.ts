01dc6a94648e1ad62bb65b6f53e656a7c5a6219a
June Wu <jewu19@fareastone.com.tw>
2026-06-12 14:39:31 +0800

    fix: version

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,33 +1,40 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  devtools: { enabled: true },
-  css: ["~/assets/css/main.css", "animate.css"],
-  plugins: [{ src: "@/plugins/aos", ssr: false, mode: "client" }],
-  postcss: {
-    plugins: {
-      autoprefixer: {},
-    },
-  },
-  modules: [
-    // ...
-    "@pinia/nuxt",
-  ],
-  app: {
-    head: {
-      htmlAttrs: {
-        lang: "en"
-      },
-      link: [
-        {
-          rel: "stylesheet",
-          href: "https://use.typekit.net/wel8ibk.css",
-        },
-      ],
-      script: [
-        {
-          src: "https://www.youtube.com/iframe_api",
-        },
-      ],
-    },
-  },
-});
+	devtools: { enabled: true },
+	css: ["~/assets/css/main.css", "animate.css"],
+	plugins: ["~/plugins/aos.client"],
+	postcss: {
+		plugins: {
+			autoprefixer: {},
+		},
+	},
+	experimental: {
+		// 關閉 payloadExtraction 減少對 build/meta 雜湊 JSON 的依賴
+		payloadExtraction: false,
+	},
+	nitro: {
+		preset: "vercel", // 強制指定為 vercel 預設環境
+	},
+	modules: [
+		// ...
+		"@pinia/nuxt",
+	],
+	app: {
+		head: {
+			htmlAttrs: {
+				lang: "en",
+			},
+			link: [
+				{
+					rel: "stylesheet",
+					href: "https://use.typekit.net/wel8ibk.css",
+				},
+			],
+			script: [
+				{
+					src: "https://www.youtube.com/iframe_api",
+				},
+			],
+		},
+	},
+})
