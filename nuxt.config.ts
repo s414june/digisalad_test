0607547838c5ccceb937117f482af50c121fea80
June Wu <jewu19@fareastone.com.tw>
2026-06-12 14:41:22 +0800

    fix setting

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,7 +2,7 @@
 export default defineNuxtConfig({
 	devtools: { enabled: true },
 	css: ["~/assets/css/main.css", "animate.css"],
-	plugins: ["~/plugins/aos.client"],
+	plugins: [{ src: "@/plugins/aos", ssr: false, mode: "client" }],
 	postcss: {
 		plugins: {
 			autoprefixer: {},
